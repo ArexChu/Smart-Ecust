e4c27a4a026ed5fd8b6556d7517e3fa65abbee37
Arex <arexchu9@gmail.com>
2017-08-24 15:50:14 +0800

    applicationId to ecust

---
+++ mobile/src/main/java/org/openhab/ecust/util/MyHttpClient.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2010-2016, openHAB.org and others.
+ *
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.openhab.ecust.util;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.security.cert.CertificateException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Call;
+import okhttp3.Credentials;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+
+public abstract class MyHttpClient<T> {
+
+    private static final String TAG = MyHttpClient.class.getSimpleName();
+
+    public interface ResponseHandler {
+        void onFailure(Call call, int statusCode, Headers headers, byte[] responseBody, Throwable error);
+        void onSuccess(Call call, int statusCode, Headers headers, byte[] responseBody);
+    }
+
+    public interface TextResponseHandler {
+        void onFailure(Call call, int statusCode, Headers headers, String responseBody, Throwable error);
+        void onSuccess(Call call, int statusCode, Headers headers, String responseBody);
+    }
+
+    protected Map<String, String> headers = new HashMap<String, String>();
+    protected OkHttpClient.Builder clientBuilder = new OkHttpClient.Builder();
+    protected OkHttpClient client = clientBuilder.build();
+
+    protected void clientSSLSetup(Boolean ignoreSSLHostname, Boolean ignoreCertTrust) {
+        if (ignoreSSLHostname) {
+            clientBuilder.hostnameVerifier(org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+            client = clientBuilder.build();
+        }
+        if (ignoreCertTrust) {
+            X509TrustManager trustAllCertsTrustManager =
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                    }
+
+                    @Override
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+                    }
+
+                    @Override
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new java.security.cert.X509Certificate[]{};
+                    }
+                };
+
+            try {
+                final SSLContext sslContext = SSLContext.getInstance("TLS");
+                sslContext.init(null, new TrustManager[] { trustAllCertsTrustManager }, new java
+                        .security.SecureRandom());
+                final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+                clientBuilder.sslSocketFactory(sslSocketFactory, trustAllCertsTrustManager);
+            } catch (Exception e) {
+                Log.d(TAG, "Applying certificate trust settings failed", e);
+            }
+        }
+        client = clientBuilder.build();
+    }
+
+    public void setBasicAuth(String username, String password) {
+        setBasicAuth(username, password, false);
+    }
+
+    public void setBasicAuth(final String username, final String password, boolean preemtive) {
+        String credential = Credentials.basic(username, password);
+        headers.put("Authorization", credential);
+    }
+
+    public void setTimeout(int timeout) {
+        clientBuilder.readTimeout(timeout, TimeUnit.MILLISECONDS);
+        client = clientBuilder.build();
+    }
+
+    public void addHeader(String key, String value) {
+        headers.put(key, value);
+    }
+
+    public T get(String url, ResponseHandler responseHandler) {
+        return method(url, "GET", null, null, null, responseHandler);
+    }
+
+    public T get(String url, TextResponseHandler textResponseHandler) {
+        return get(url, getResponseHandler(textResponseHandler));
+    }
+
+    public T get(String url, Map<String, String> headers, ResponseHandler responseHandler) {
+        return method(url, "GET", headers, null, null, responseHandler);
+    }
+
+    public T post(String url, String requestBody, String mediaType, ResponseHandler responseHandler) {
+        return method(url, "POST", null, requestBody, mediaType, responseHandler);
+    }
+
+    public T post(String url, String requestBody, String mediaType, TextResponseHandler textResponseHandler) {
+        return post(url, requestBody, mediaType, getResponseHandler(textResponseHandler));
+    }
+
+    public T delete(String url, ResponseHandler responseHandler) {
+        return method(url, "DELETE", null, null, null, responseHandler);
+    }
+
+    protected abstract T method(String url, String method, Map<String, String> addHeaders,
+                                   String requestBody, String mediaType, final ResponseHandler
+                                           responseHandler);
+
+    @NonNull
+    private ResponseHandler getResponseHandler(final TextResponseHandler textResponseHandler) {
+        return new ResponseHandler() {
+            @Override
+            public void onFailure(Call call, int statusCode, Headers headers, byte[] responseBody, Throwable error) {
+                try {
+                    String responseString = responseBody == null ? null : new String(responseBody, "UTF-8");
+                    textResponseHandler.onFailure(call, statusCode, headers, responseString, error);
+                } catch (UnsupportedEncodingException e) {
+                    textResponseHandler.onFailure(call, statusCode, headers, null, e);
+                }
+            }
+
+            @Override
+            public void onSuccess(Call call, int statusCode, Headers headers, byte[] responseBody) {
+                try {
+                    String responseString = responseBody == null ? null : new String(responseBody, "UTF-8");
+                    textResponseHandler.onSuccess(call, statusCode, headers, responseString);
+                } catch (UnsupportedEncodingException e) {
+                    textResponseHandler.onFailure(call, statusCode, headers, null, e);
+                }
+            }
+        };
+    }
+}
---
+++ mobile/src/main/java/org/openhab/ecust/util/MyWebImage.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2010-2016, openHAB.org and others.
+ *
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.openhab.ecust.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+import com.loopj.android.image.SmartImage;
+import com.loopj.android.image.WebImageCache;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+
+public class MyWebImage implements SmartImage {
+    private static final String TAG = "MyWebImage";
+    private static final int CONNECT_TIMEOUT = 5000;
+    private static final int READ_TIMEOUT = 10000;
+
+    private static WebImageCache webImageCache;
+
+    private String url;
+    private boolean useCache = true;
+    
+    private String authUsername;
+    private String authPassword;
+    private boolean shouldAuth = false;
+
+    public MyWebImage(String url, String username, String password) {
+        this(url, true, username, password);
+    }
+
+    public MyWebImage(String url, boolean useCache, String username, String password) {
+    	this.url = url;
+    	this.useCache = useCache;
+        this.setAuthentication(username, password);
+    }
+    
+    public Bitmap getBitmap(Context context) {
+        // Don't leak context
+        if(webImageCache == null) {
+            webImageCache = new WebImageCache(context);
+        }
+
+        // Try getting bitmap from cache first
+        Bitmap bitmap = null;
+        if(url != null) {
+            if (this.useCache)
+            	bitmap = webImageCache.get(url);
+            if(bitmap == null) {
+            	Log.i("MyWebImage", "Cache for " + url + " is empty, getting image");
+                final String iconFormat = PreferenceManager.getDefaultSharedPreferences(context).getString("iconFormatType","PNG");
+                bitmap = getBitmapFromUrl(context, url, iconFormat);
+                if(bitmap != null && this.useCache) {
+                    webImageCache.put(url, bitmap);
+                }
+            }
+        }
+
+        return bitmap;
+    }
+
+    private Bitmap getBitmapFromUrl(Context context, final String url, final String iconFormat) {
+        final Map<String, Object> result = new HashMap<String, Object>();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        MyAsyncHttpClient client = new MyAsyncHttpClient(prefs.getBoolean(Constants
+                .PREFERENCE_SSLHOST, false), prefs.getBoolean(Constants.PREFERENCE_SSLCERT, false));
+        client.setTimeout(READ_TIMEOUT);
+        if (shouldAuth) {
+            client.setBasicAuth(authUsername, authPassword);
+        }
+
+        client.get(url, new MyHttpClient.ResponseHandler() {
+            @Override
+            public void onFailure(Call call, int statusCode, Headers headers, byte[] responseBody, Throwable error) {
+                Log.e(TAG, "Failed to get " + url + " with code " + statusCode + ":" + error);
+                synchronized (result) {
+                    result.put("error", error);
+                    result.notify();
+                }
+            }
+
+            @Override
+            public void onSuccess(Call call, int statusCode, Headers headers, byte[] responseBody) {
+                InputStream is = new ByteArrayInputStream(responseBody);
+                synchronized (result) {
+                    result.put("bitmap", getBitmapFromInputStream(iconFormat, is));
+                    result.notify();
+                }
+            }
+        });
+
+        synchronized (result) {
+            try {
+                result.wait(60000);
+            } catch (InterruptedException e) {
+                Log.e(TAG, "Timeout fetching " + url);
+                return null;
+            }
+
+            if (result.containsKey("error")) {
+                return null;
+            }
+
+        }
+
+        Log.i(TAG, "fetched bitmap for " + url);
+
+        return (Bitmap)result.get("bitmap");
+    }
+
+    private Bitmap getBitmapFromInputStream(String iconFormat, InputStream is) {
+        Bitmap bitmap;
+        if("SVG".equals(iconFormat)) {
+            bitmap = getBitmapFromSvgInputstream(is);
+        }else {
+            bitmap = BitmapFactory.decodeStream(is);
+        }
+        return bitmap;
+    }
+
+    private Bitmap getBitmapFromSvgInputstream(InputStream is) {
+        Bitmap bitmap = null;
+        try {
+            SVG svg = SVG.getFromInputStream(is);
+                double width = svg.getDocumentViewBox().width();
+                double height = svg.getDocumentViewBox().height();
+
+                bitmap = Bitmap.createBitmap((int) Math.ceil(width), (int) Math.ceil(height), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(bitmap);
+                canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);//drawARGB(0,0,0,0);//drawRGB(255, 255, 255);
+                svg.renderToCanvas(canvas);
+        } catch (SVGParseException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    public static void removeFromCache(String url) {
+        if(webImageCache != null) {
+            webImageCache.remove(url);
+        }
+    }
+    
+    public void setAuthentication(String username, String password) {
+    	this.authUsername = username;
+    	this.authPassword = password;
+    	if (this.authUsername != null && (this.authUsername.length() > 0 && this.authPassword.length() > 0))
+    		this.shouldAuth = true;
+    }
+}
